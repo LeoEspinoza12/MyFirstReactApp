d2978c0797178e9989561fb0c5337ac11b35b01f
Leo espinoza <leo.spinoza@yahoo.com>
2018-10-09 21:34:58 -0500

    added life cycle hooks

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -20,6 +20,22 @@ componentDidMount() {
   console.log('[Persons.js] Inside componentDidMount')
 }
 
+componentWillReceiveProps(nextProps) {
+  console.log('[Update Persons.js] Inside componentWillReceive', nextProps)
+}
+
+shouldComponentUpdate(nextProps, nextState){
+  console.log('[Update Persons.js] Inside shouldComponentsUpdate Persons.Js', nextProps, nextState)
+  return nextProps.persons !== this.props.persons
+}
+
+componentWillUpdate(nextProps, nextState) {
+  console.log('[Update Persons.js] Inside componentWillUpdate Persons.Js', nextProps, nextState)
+}
+
+componentDidUpdate(nextProps, nextState) {
+  console.log('[Update Persons.js] Inside componentDidUpdate Persons.Js', nextProps, nextState)
+}
   render () {
     console.log('[Persons.js] Inseide REnder')
     return this.props.persons.map( (person, index) => {
